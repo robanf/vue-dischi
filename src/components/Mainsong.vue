869df30da5c6c5b217929f54327627440d7f1353
Roberto <robertoanfuso98@gmail.com>
2021-07-10 18:11:16 +0200

    problemi con il filtro

--- src/components/Mainsong.vue
+++ src/components/Mainsong.vue
@@ -1,8 +1,19 @@
 <template>
   <div class="my-main">
       <div class="container">
+          <div class="row">
+              <div class="col">
+                    <select name="generi" id="generi" v-model="franco" :onchange="filtro()">
+                        <option value="All">All</option>
+                        <option value="Rock">Rock</option>
+                        <option value="Pop">Pop</option>
+                        <option value="Jazz">Jazz</option>
+                        <option value="Metal">Metal</option>
+                    </select>
+                </div>
+          </div>
           <div class="row g-3">
-              <div v-for="(song,index) in songlist" :key="index" class="col-3 my-song">
+              <div v-for="(song,index) in songlist" :key="index" class="col-3 my-song" >
                   <img :src="song.poster" alt="">
                   <div class="fs-5 fw-bolder">{{song.title}}</div>
                   <div class="my-text">{{song.author}}</div>
@@ -20,11 +31,23 @@ export default {
     data(){
         return{
             apiUrl:"https://flynn.boolean.careers/exercises/api/array/music",
-            songlist:[]
+            songlist:[],
+            franco:"All",
+            filtrato:"",
         }
     },
     created(){
         this.getSong();
+    },
+    computed:{
+        filtersong(){
+            return this.songlist.filter( item =>{
+                return console.log(item.genre.includes(this.filtrato));
+            })
+        },
+        Test(){
+            return console.log(this.franco);
+        }
     },
     methods:{
         getSong(){
@@ -35,6 +58,16 @@ export default {
                     this.songlist = response.data.response;
                     console.log(this.songlist);
                 })
+        },
+        filtro(){
+
+            if(this.franco=="All"){
+                this.filtrato="";
+            }
+            else{
+                this.filtrato=this.franco;
+            }
+            console.log(this.filtrato)
         }
     }
 }
